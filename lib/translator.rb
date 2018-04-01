97e2ca2382e3ca485c533cbfc0515245f3824ba1
Ian Pollack <ianjpollack@gmail.com>
2018-04-01 18:50:20 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,13 +5,13 @@ emoticons = YAML.load_file("./lib/emoticons.yml")
 # require modules here
 
 def load_library(emoticons)
-    binding.pry
+  #  binding.pry
   library_hash = {}
   library_hash['get_meaning'] = {}
   library_hash['get_emoticon'] = {}
   library_hash['get_meaning']
 
-binding.pry
+#binding.pry
 
   library_hash
 end
